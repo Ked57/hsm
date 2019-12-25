d5e989e4f15a0cc1b81b1dbc3f0259debe50c14a
Ked57 <kedketh@gmail.com>
2019-12-25 15:10:45 +0100

    :heavy_plus_sign: Add vuex persist

--- webapp/src/store.ts
+++ webapp/src/store.ts
@@ -1,10 +1,16 @@
 import Vue from "vue";
+import VuexPersistence from 'vuex-persist'
 import Vuex from "vuex";
 
 Vue.use(Vuex);
 
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage
+})
+
 export default new Vuex.Store({
   state: {},
   mutations: {},
-  actions: {}
+  actions: {},
+  plugins: [vuexLocal.plugin]
 });
