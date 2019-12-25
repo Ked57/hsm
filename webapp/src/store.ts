951c87a203c8e017f9264dc10fe1b5b94825d5d0
Ked57 <kedketh@gmail.com>
2019-12-25 17:55:24 +0100

    Format fix

--- webapp/src/store.ts
+++ webapp/src/store.ts
@@ -1,12 +1,12 @@
 import Vue from "vue";
-import VuexPersistence from 'vuex-persist'
+import VuexPersistence from "vuex-persist";
 import Vuex from "vuex";
 
 Vue.use(Vuex);
 
 const vuexLocal = new VuexPersistence({
   storage: window.localStorage
-})
+});
 
 export default new Vuex.Store({
   state: {},
