355b0e45e1fecd50bd7e4e8367be83a4797a801d
Ked57 <kedketh@gmail.com>
2019-12-26 18:27:28 +0100

    Format fix

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -5,7 +5,7 @@ import store from "./store";
 import "./registerServiceWorker";
 import router from "./router";
 
-import './assets/styles/index.css';
+import "./assets/styles/index.css";
 
 Vue.config.productionTip = false;
 Vue.use(VueCompositionApi);
