6c9eb30475beb4789896eb986954c6ebf02bf132
Ked57 <kedketh@gmail.com>
2019-12-26 20:17:58 +0100

    Add authentication

--- serverapp/src/app.ts
+++ serverapp/src/app.ts
@@ -1,7 +1,17 @@
 import fastify from "fastify";
+import cors from "fastify-cors";
+import { Bearer } from "permit";
+import { authorize } from "./util/authorization";
 
 export const main = async () => {
   const app = fastify({ logger: { prettyPrint: true } });
+
+  const permit = new Bearer({});
+
+  app.addHook("preHandler", (req, res) => authorize(permit, req, res));
+
+  app.register(cors, { origin: "*" });
+
   const address = await app.listen(
     Number(process.env.PORT || "3000"),
     process.env.HOST || "localhost"
---
+++ serverapp/src/util/authorization.ts
@@ -0,0 +1,17 @@
+import { Bearer } from "permit";
+import fastify = require("fastify");
+import { ServerResponse } from "http";
+
+export const authorize = async (permit: Bearer, request: fastify.FastifyRequest, reply: fastify.FastifyReply<ServerResponse>) => {
+    const token = permit.check(request.raw);
+
+    if (process.env.SECURITY_KEY && !token) {
+      permit.fail(reply.res);
+      throw new Error("Authentication required!");
+    }
+
+    if (process.env.SECURITY_KEY && token !== process.env.SECURITY_KEY) {
+      permit.fail(reply.res);
+      throw new Error("Authentication invalid!");
+    }
+  }
