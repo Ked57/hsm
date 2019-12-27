a6f8af839783a7f6a29318386db65deb716d8d41
Ked57 <kedketh@gmail.com>
2019-12-27 20:27:22 +0100

    Update tests

---
+++ webapp/tests/e2e/specs/home_page.js
@@ -0,0 +1,18 @@
+describe('The home page', () => {
+  it('Visits the home URL', () => {
+    cy.visit('/')
+    cy.contains('h1', 'Home Server Manager')
+  })
+  it('Visits the home URL', () => {
+    cy.visit('/')
+    cy.contains('h1', 'Server status')
+  })
+  it('Visits the home URL', () => {
+    cy.visit('/')
+    cy.contains('button', 'On / Off')
+  })
+  it('Visits the home URL', () => {
+    cy.visit('/')
+    cy.contains('button', 'Add an action')
+  })
+})
