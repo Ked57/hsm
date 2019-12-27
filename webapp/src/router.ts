023d1e2ad3dabdbbfc99b332f451cd5b7fa07b1f
Ked57 <kedketh@gmail.com>
2019-12-28 00:22:38 +0100

    Add login and error designs + routing

--- webapp/src/router.ts
+++ webapp/src/router.ts
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import Home from "./components/Home";
 import Error from "./components/Error";
+import Login from "./components/Login";
 
 Vue.use(Router);
 
@@ -11,6 +12,11 @@ export default new Router({
       path: "/",
       name: "home",
       component: Home
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: Login
     },
     {
       path: "*",
