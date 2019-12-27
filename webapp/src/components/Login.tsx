67857ecfaa8da6ebfcc3678be9e37039358ec44c
Ked57 <kedketh@gmail.com>
2019-12-28 00:49:01 +0100

    Add basic tests

--- webapp/src/components/Login.tsx
+++ webapp/src/components/Login.tsx
@@ -7,9 +7,9 @@ export default createComponent({
       <div id="Login" class="flex flex-col justify-center items-center">
         <h1 class="text-4xl p-5 text-center gradient-text">Login</h1>
         <span class="text-xl p-5 gradient-text">URL / IP</span>
-        <input class="rounded-lg m-4 p-2 max-w-xl w-11/12 text-xl text-center h-16 bg-gray-400"></input>
+        <input id="address-input" class="rounded-lg m-4 p-2 max-w-xl w-11/12 text-xl text-center h-16 bg-gray-400"></input>
         <span class="text-xl p-5 gradient-text">Security Code</span>
-        <input type="password" class="rounded-lg m-4 p-2 max-w-xl w-11/12 text-xl text-center h-16 bg-gray-400"></input>
+        <input id="security-input" type="password" class="rounded-lg m-4 p-2 max-w-xl w-11/12 text-xl text-center h-16 bg-gray-400"></input>
         <button
           class="gradient-container rounded-lg m-4 p-2 max-w-xl w-11/12 text-xl text-center h-16"
           onClick={() => console.log("login")}
---
+++ webapp/tests/e2e/specs/login_page.js
@@ -0,0 +1,27 @@
+describe('The login page', () => {
+    it('Visits the login URL', () => {
+      cy.visit('#/login')
+      cy.contains('h1', 'Login')
+    })
+    it('Visits the login URL', () => {
+      cy.visit('#/login')
+      cy.contains('span', 'URL / IP')
+    })
+    it('Visits the login URL', () => {
+      cy.visit('#/login')
+      cy.get('#address-input')
+    })
+    it('Visits the login URL', () => {
+      cy.visit('#/login')
+      cy.contains('span', 'Security Code')
+    })
+    it('Visits the login URL', () => {
+      cy.visit('#/login')
+      cy.get('#security-input')
+    })
+    it('Visits the login URL', () => {
+      cy.visit('#/login')
+      cy.contains('button', 'Login')
+    })
+  })
+  
