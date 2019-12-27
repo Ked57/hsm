25032c16005d101d950020a2cbe5e352df4ebf9f
Clément Fassot <kedketh@gmail.com>
2019-12-27 20:35:16 +0100

    :lipstick: Add basic home design (#18)

--- webapp/src/components/Home.tsx
+++ webapp/src/components/Home.tsx
@@ -1,13 +1,17 @@
 import { createComponent } from "@vue/composition-api";
-import HelloWorld from './HelloWorld';
+import ServerStatus from './ServerStatus';
+import ActionButton from './ActionButton';
 
 
 export default createComponent({
   name: "Home",
   setup() {
     return () => (
-      <div id="Home">
-        <HelloWorld msg="Welcome to Your Vue.js + TypeScript PWA" />
+      <div id="Home" class="flex flex-col justify-center items-center">
+        <h1 class="text-4xl p-5 text-center gradient-text">Home Server Manager</h1>
+        <ServerStatus />
+        <ActionButton text="On / Off" action="" />
+        <ActionButton text="Add an action" action="" />
       </div>
     );
   }
