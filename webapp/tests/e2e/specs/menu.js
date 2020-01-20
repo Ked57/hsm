bbd718ac2389692606e41eeb2892c09758c5464e
ked57 <kedketh@gmail.com>
2020-01-20 09:46:52 +0100

    Add menu test

---
+++ webapp/tests/e2e/specs/menu.js
@@ -0,0 +1,8 @@
+describe("The menu page", () => {
+    it("Open the menu", () => {
+      cy.visit("/");
+      cy.get("#showMenuButton").click()
+      cy.contains("Button", "Home")
+    });
+  });
+  
